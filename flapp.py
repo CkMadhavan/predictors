55b786181f9baf24377bf2d60ed3b9176e11d5be
Saber Technologies <33689397+CkMadhavan@users.noreply.github.com>
2020-04-03 20:30:55 +0530

    Update flapp.py

--- flapp.py
+++ flapp.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , request
+from flask import render_template , stream_with_context, request, Response , Flask
 
 app = Flask(__name__)
 
@@ -36,138 +36,132 @@ class NumpyArrayEncoder(JSONEncoder):
 
 @app.route('/<params>')
 def index(params):
+    def generate():
+        start_time = time.time()
     
-    start_time = time.time()
-    
-    firebase_app = firebase.FirebaseApplication('https://predictors-a4804.firebaseio.com', None)
+        firebase_app = firebase.FirebaseApplication('https://predictors-a4804.firebaseio.com', None)
+        yield 'Hi '
 
-    print('Initialised Firebase App')
+        yield 'Initialised Firebase App '
 
-    Usr = params.split('-')[0]
-    Predictor = params.split('-')[1]
-    
-    print(params)
-    print(Usr)
-    print(Predictor)
+        Usr = params.split('-')[0]
+        Predictor = params.split('-')[1]
 
-    categories = []
-    vids = []
+        categories = []
+        vids = []
 
-    result = firebase_app.get('/'+Usr+'/'+Predictor+'/Classes', None)
+        result = firebase_app.get('/'+Usr+'/'+Predictor+'/Classes', None)
 
-    for i in result:
-        categories.append(i)
-        vids_of_cat = []
-        for j in result[i]:
-            vids_of_cat.append(result[i][j])
-        vids.append(vids_of_cat)
+        for i in result:
+            categories.append(i)
+            vids_of_cat = []
+            for j in result[i]:
+                vids_of_cat.append(result[i][j])
+            vids.append(vids_of_cat)
 
-    training = []
+        training = []
 
-    for i in range(len(categories)):
-        for vid in vids[i]:
+        for i in range(len(categories)):
+            for vid in vids[i]:
 
-            vcap = cv2.VideoCapture(vid)
+                vcap = cv2.VideoCapture(vid)
 
-            while(True):
-                ret, frame = vcap.read()
-                if frame is not None:
-                    img = cv2.resize(frame,(100,100))
-                    img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB)
-                    img = cv2.cvtColor(img , cv2.COLOR_RGB2GRAY)
-                    img = img.reshape(1,100,100)
-                    training.append([img , i])
-                else:
-                    break
+                while(True):
+                    ret, frame = vcap.read()
+                    if frame is not None:
+                        img = cv2.resize(frame,(100,100))
+                        img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB)
+                        img = cv2.cvtColor(img , cv2.COLOR_RGB2GRAY)
+                        img = img.reshape(1,100,100)
+                        training.append([img , i])
+                    else:
+                        break
 
-            vcap.release()
-            cv2.destroyAllWindows()
+                vcap.release()
+                cv2.destroyAllWindows()
 
-    print(len(training))
-    shuffle(training)
+        shuffle(training)
 
-    print('Collected Training From Videos')
+        yield 'Collected Training From Videos '
 
-    imgs , cats = [] , []
+        imgs , cats = [] , []
 
-    for f,l in training:
-        imgs.append(f)
-        cats.append(l)
+        for f,l in training:
+            imgs.append(f)
+            cats.append(l)
+            
+        X_train = np.asarray(imgs)
+        Y_train = np.asarray(cats)
+
+        classes = len(categories)
+        imgs_per_class=150
+
+        dataset = []
+
+        for a in range(classes):
+            for image in [X_train[i] for i in range(len(X_train)) if Y_train[i] == a][:imgs_per_class]:
+                dataset.append([image , a])
+
+        shuffle(dataset)
+
+        imgs = []
+        cats = []
+
+        for x in dataset:
+            imgs.append(np.repeat(x[0][..., np.newaxis], 3, -1))
+            cats.append(x[1])
+
+        X = np.array(imgs).reshape(-1, 100, 100, 3)
+        Y = to_categorical(cats , classes)
+
+        del training
+        del dataset
+        del X_train
+        del Y_train
+
+        datagen = keras.preprocessing.image.ImageDataGenerator(
+            rotation_range=40,
+                zoom_range=0.2,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.1,
+                brightness_range=(0.25, 0.75),
+                horizontal_flip=True,
+                fill_mode="nearest")
+
+        datagen.fit(X)
+
+        base = keras.models.load_model('base_model.h5')
+
+        yield 'Base Model Loaded '
+
+        model = classification_model(base , classes)
+
+        model.fit_generator(datagen.flow(X , Y , batch_size = 32) , steps_per_epoch = len(X)//32 , epochs = 1, shuffle = True , verbose=0)
+
+        yield 'Model Trained '
+
+        model.save('model_book_bottle_ball.h5')
+
+        P = model.get_weights()
+
+        yield 'Model Saving In Firebase '
+
+        numpyArrayOne = np.array(P)
+
+        numpyData = {"array": numpyArrayOne}
+        encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)
+
+        for x in [encodedNumpyData[i:i+10000000] for i in range(0, len(encodedNumpyData), 10000000)]:
+            firebase_app.post('/'+Usr+'/'+Predictor+'/Weights', x)
         
-    X_train = np.asarray(imgs)
-    Y_train = np.asarray(cats)
-
-    classes = len(categories)
-    imgs_per_class=150
-
-    dataset = []
-
-    for a in range(classes):
-        for image in [X_train[i] for i in range(len(X_train)) if Y_train[i] == a][:imgs_per_class]:
-            dataset.append([image , a])
-
-    shuffle(dataset)
-
-    imgs = []
-    cats = []
-
-    for x in dataset:
-        imgs.append(np.repeat(x[0][..., np.newaxis], 3, -1))
-        cats.append(x[1])
-
-    X = np.array(imgs).reshape(-1, 100, 100, 3)
-    Y = to_categorical(cats , classes)
-
-    del training
-    del dataset
-    del X_train
-    del Y_train
-
-    datagen = keras.preprocessing.image.ImageDataGenerator(
-        rotation_range=40,
-            zoom_range=0.2,
-            width_shift_range=0.2,
-            height_shift_range=0.2,
-            shear_range=0.1,
-            brightness_range=(0.25, 0.75),
-            horizontal_flip=True,
-            fill_mode="nearest")
-
-    datagen.fit(X)
-
-    base = keras.models.load_model('base_model.h5')
-
-    print('Base Model Loaded')
-
-    model = classification_model(base , classes)
-
-    model.fit_generator(datagen.flow(X , Y , batch_size = 32) , steps_per_epoch = len(X)//32 , epochs = 1, shuffle = True , verbose=1)
-
-    print('Model Trained')
-
-    model.save('model_book_bottle_ball.h5')
-
-    P = model.get_weights()
-
-    print('Model Saving In Firebase')
-
-    numpyArrayOne = np.array(P)
-
-    numpyData = {"array": numpyArrayOne}
-    encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)
-
-    for x in [encodedNumpyData[i:i+10000000] for i in range(0, len(encodedNumpyData), 10000000)]:
-        firebase_app.post('/'+Usr+'/'+Predictor+'/Weights', x)
+        yield 'Model Saved In Firebase '
+        
+        K.clear_session()
+        
+        yield "Execution Time = "+str(time.time() - start_time)+" Seconds "
     
-    print('Model Saved In Firebase')
-    
-    K.clear_session()
-    
-    print("Execution Time = %s Seconds" % (time.time() - start_time))
-    
-    
-    return 'Done'
-
+    return Response(stream_with_context(generate())) 
 
 if __name__ == "__main__":
     app.run()
